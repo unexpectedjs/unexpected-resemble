1872023869c599d5fefe853fd852caff97462c32
Andreas Lind <andreaslindpetersen@gmail.com>
2018-10-31 21:30:35 +0100

    eslint --fix .

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,11 +1,12 @@
 /*global unexpected:true*/
 var proxyquire = require('proxyquire');
-unexpected = require('unexpected')
-    .installPlugin(proxyquire('./lib/unexpectedResemble', {
-        'magicpen-media': proxyquire('magicpen-media', {
-            gettemporaryfilepath: function (options) {
-                return '/tmp/image' + ((options && options.suffix) || '');
-            }
-        })
-    }));
+unexpected = require('unexpected').installPlugin(
+  proxyquire('./lib/unexpectedResemble', {
+    'magicpen-media': proxyquire('magicpen-media', {
+      gettemporaryfilepath: function(options) {
+        return '/tmp/image' + ((options && options.suffix) || '');
+      }
+    })
+  })
+);
 unexpected.output.preferredWidth = 80;
