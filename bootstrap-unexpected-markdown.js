480b5a2f15c58f61f3153814f08c099aa0d15fe6
Andreas Lind <andreaslindpetersen@gmail.com>
2019-01-01 21:51:44 +0100

    expect.clone() before installing plugins (to be compatible with Unexpected 11)

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,12 +1,14 @@
 /*global unexpected:true*/
 var proxyquire = require('proxyquire');
-unexpected = require('unexpected').installPlugin(
-  proxyquire('./lib/unexpectedResemble', {
-    'magicpen-media': proxyquire('magicpen-media', {
-      gettemporaryfilepath: function(options) {
-        return '/tmp/image' + ((options && options.suffix) || '');
-      }
+unexpected = require('unexpected')
+  .clone()
+  .installPlugin(
+    proxyquire('./lib/unexpectedResemble', {
+      'magicpen-media': proxyquire('magicpen-media', {
+        gettemporaryfilepath: function(options) {
+          return '/tmp/image' + ((options && options.suffix) || '');
+        }
+      })
     })
-  })
-);
+  );
 unexpected.output.preferredWidth = 80;
